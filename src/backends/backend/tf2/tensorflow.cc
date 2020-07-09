d83b1f3a31e3643452c32d85789dce60ecf7cfdc
GuanLuo <gluo@nvidia.com>
2020-07-09 15:00:11 -0700

    WIP

---
+++ src/backends/backend/tf2/tensorflow.cc
@@ -0,0 +1,271 @@
+// Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "src/backends/backend/examples/backend_utils.h"
+
+#include <atomic>
+#include <chrono>
+#include <memory>
+#include <thread>
+
+namespace ni = nvidia::inferenceserver;
+namespace nib = nvidia::inferenceserver::backend;
+
+//
+// WIP: TF2 Backend that implements the TRITONBACKEND API.
+//
+
+namespace {
+
+#define RESPOND_AND_RETURN_IF_ERROR(REQUEST, X)                         \
+  do {                                                                  \
+    TRITONSERVER_Error* rarie_err__ = (X);                              \
+    if (rarie_err__ != nullptr) {                                       \
+      TRITONBACKEND_Response* rarie_response__ = nullptr;               \
+      LOG_IF_ERROR(                                                     \
+          TRITONBACKEND_ResponseNew(&rarie_response__, REQUEST),        \
+          "failed to create response");                                 \
+      if (rarie_response__ != nullptr) {                                \
+        LOG_IF_ERROR(                                                   \
+            TRITONBACKEND_ResponseSend(                                 \
+                rarie_response__, TRITONSERVER_RESPONSE_COMPLETE_FINAL, \
+                rarie_err__),                                           \
+            "failed to send error response");                           \
+      }                                                                 \
+      TRITONSERVER_ErrorDelete(rarie_err__);                            \
+      return;                                                           \
+    }                                                                   \
+  } while (false)
+
+#define RESPOND_FACTORY_AND_RETURN_IF_ERROR(FACTORY, X)                      \
+  do {                                                                       \
+    TRITONSERVER_Error* rfarie_err__ = (X);                                  \
+    if (rfarie_err__ != nullptr) {                                           \
+      TRITONBACKEND_Response* rfarie_response__ = nullptr;                   \
+      LOG_IF_ERROR(                                                          \
+          TRITONBACKEND_ResponseNewFromFactory(&rfarie_response__, FACTORY), \
+          "failed to create response");                                      \
+      if (rfarie_response__ != nullptr) {                                    \
+        LOG_IF_ERROR(                                                        \
+            TRITONBACKEND_ResponseSend(                                      \
+                rfarie_response__, TRITONSERVER_RESPONSE_COMPLETE_FINAL,     \
+                rfarie_err__),                                               \
+            "failed to send error response");                                \
+      }                                                                      \
+      TRITONSERVER_ErrorDelete(rfarie_err__);                                \
+      return;                                                                \
+    }                                                                        \
+  } while (false)
+
+
+//
+// ModelState
+//
+// State associated with a model that is using this backend. An object
+// of this class is created and associated with each
+// TRITONBACKEND_Model.
+//
+class ModelState {
+ public:
+  static TRITONSERVER_Error* Create(
+      TRITONBACKEND_Model* triton_model, ModelState** state);
+  ~ModelState();
+
+  // Validate that model configuration is supported by this backend.
+  TRITONSERVER_Error* ValidateModelConfig();
+
+  // Spawn a thread to produce outputs for a request. Return the
+  // request wait time before it should release.
+  void ProcessRequest(TRITONBACKEND_Request* request, uint32_t* wait_ms);
+
+ private:
+  ModelState(
+      TRITONBACKEND_Model* triton_model, ni::TritonJson::Value&& model_config);
+  void ResponseThread(
+      TRITONBACKEND_ResponseFactory* factory_ptr, const int32_t* in_buffer_ptr,
+      const int32_t* delay_buffer_ptr, const uint32_t element_count);
+
+  TRITONBACKEND_Model* triton_model_;
+  ni::TritonJson::Value model_config_;
+  std::atomic<size_t> inflight_thread_count_;
+};
+
+}  // namespace
+
+/////////////
+
+extern "C" {
+
+// Implementing TRITONBACKEND_ModelInitialize is optional. The backend
+// should initialize any state that is intended to be shared across
+// all instances of the model.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelInitialize(TRITONBACKEND_Model* model)
+{
+  const char* cname;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelName(model, &cname));
+  std::string name(cname);
+
+  uint64_t version;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelVersion(model, &version));
+
+  TRITONSERVER_LogMessage(
+      TRITONSERVER_LOG_INFO, __FILE__, __LINE__,
+      (std::string("TRITONBACKEND_ModelInitialize: ") + name + " (version " +
+       std::to_string(version) + ")")
+          .c_str());
+
+  // With each model we create a ModelState object and associate it
+  // with the TRITONBACKEND_Model.
+  ModelState* model_state;
+  RETURN_IF_ERROR(ModelState::Create(model, &model_state));
+  RETURN_IF_ERROR(
+      TRITONBACKEND_ModelSetState(model, reinterpret_cast<void*>(model_state)));
+
+  // One of the primary things to do in ModelInitialize is to examine
+  // the model configuration to ensure that it is something that this
+  // backend can support. If not, returning an error from this
+  // function will prevent the model from loading.
+  RETURN_IF_ERROR(model_state->ValidateModelConfig());
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelFinalize is optional unless state
+// is set using TRITONBACKEND_ModelSetState. The backend must free
+// this state and perform any other cleanup.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelFinalize(TRITONBACKEND_Model* model)
+{
+  void* vstate;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelState(model, &vstate));
+  ModelState* model_state = reinterpret_cast<ModelState*>(vstate);
+
+  TRITONSERVER_LogMessage(
+      TRITONSERVER_LOG_INFO, __FILE__, __LINE__,
+      "TRITONBACKEND_ModelFinalize: delete model state");
+
+  delete model_state;
+
+  return nullptr;  // success
+}
+
+// Implementing TRITONBACKEND_ModelExecute is required.
+TRITONSERVER_Error*
+TRITONBACKEND_ModelExecute(
+    TRITONBACKEND_Model* model, TRITONBACKEND_Request** requests,
+    const uint32_t request_count)
+{
+  const char* model_name;
+  RETURN_IF_ERROR(TRITONBACKEND_ModelName(model, &model_name));
+
+  TRITONSERVER_LogMessage(
+      TRITONSERVER_LOG_INFO, __FILE__, __LINE__,
+      (std::string("TRITONBACKEND_ModelExecute: model ") + model_name +
+       " with " + std::to_string(request_count) + " requests")
+          .c_str());
+
+  // Triton only calls model execute from a single thread at a time
+  // *for a given model*. But since this backend could be used by
+  // multiple models the implementation needs to handle multiple
+  // models executing at the same time. Good practice for this is to
+  // use only function-local and model-specific state (obtained from
+  // 'model'), which is what we do here.
+  ModelState* state;
+  RETURN_IF_ERROR(
+      TRITONBACKEND_ModelState(model, reinterpret_cast<void**>(&state)));
+
+  // This backend does not support models that support batching, so
+  // 'request_count' should always be 1.
+  RETURN_ERROR_IF_FALSE(
+      request_count <= 1, TRITONSERVER_ERROR_INVALID_ARG,
+      std::string("repeat backend does not support batched request execution"));
+
+  uint64_t exec_start_ns = 0;
+  SET_TIMESTAMP(exec_start_ns);
+  uint32_t wait_milliseconds = 0;
+
+  // At this point we accept ownership of 'requests', which means that
+  // even if something goes wrong we must still return success from
+  // this function. If something does go wrong in processing a
+  // particular request then we send an error response just for the
+  // specific request.
+
+  // For simplicity we process each request in a separate thread.
+  for (uint32_t r = 0; r < request_count; ++r) {
+    TRITONBACKEND_Request* request = requests[r];
+    state->ProcessRequest(request, &wait_milliseconds);
+  }
+
+  // Wait, release, return...
+  TRITONSERVER_LogMessage(
+      TRITONSERVER_LOG_INFO, __FILE__, __LINE__,
+      (std::string("waiting ") + std::to_string(wait_milliseconds) +
+       " ms before releasing requests")
+          .c_str());
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(wait_milliseconds));
+
+  uint64_t exec_end_ns = 0;
+  SET_TIMESTAMP(exec_end_ns);
+
+  for (uint32_t r = 0; r < request_count; ++r) {
+    TRITONBACKEND_Request* request = requests[r];
+    // Report statistics for the request. Note that there could
+    // still be responses that have not yet been sent but those
+    // cannot be captured in the statistics as they reflect only the
+    // request object. We use the execution start/end time for
+    // compute also so that the entire execution time is associated
+    // with the inference computation.
+    LOG_IF_ERROR(
+        TRITONBACKEND_ModelReportStatistics(
+            model, request, true /* success */, TRITONBACKEND_NO_DEVICE,
+            exec_start_ns, exec_start_ns, exec_end_ns, exec_end_ns),
+        "failed reporting request statistics");
+
+    LOG_IF_ERROR(
+        TRITONBACKEND_RequestRelease(request, TRITONSERVER_REQUEST_RELEASE_ALL),
+        "failed releasing request");
+  }
+
+  // Report the entire batch statistics. This backend does not support
+  // batching so the total batch size is always 1.
+  LOG_IF_ERROR(
+      TRITONBACKEND_ModelReportBatchStatistics(
+          model, 1 /*total_batch_size*/, exec_start_ns, exec_start_ns,
+          exec_end_ns, exec_end_ns),
+      "failed reporting batch request statistics");
+
+  TRITONSERVER_LogMessage(
+      TRITONSERVER_LOG_INFO, __FILE__, __LINE__,
+      (std::string("TRITONBACKEND_ModelExecute: model ") + model_name +
+       " released " + std::to_string(request_count) + " requests")
+          .c_str());
+
+  return nullptr;  // success
+}
+
+}  // extern "C"
